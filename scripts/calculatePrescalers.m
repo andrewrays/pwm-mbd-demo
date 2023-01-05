e7dbdb5fa6d9e523050c01cd0b4776c2001a83bd
Andrew Rayman <andrewrays0702@gmail.com>
2023-01-05 18:42:35 +0300

    test: add tests for Pressing Mode Handler

--- scripts/calculatePrescalers.m
+++ scripts/calculatePrescalers.m
@@ -6,6 +6,11 @@ PRESCALER.FLTR = (1e9*Data.FLTR_PERIOD)/(1e9/Data.CLK_FREQ)/2;
 %%
 CNT_MAX = 2^Data.SIZE - 2;
 
+switch bdroot
+    case 'pwm_generator_harness'
+        Data.TYPE = get_param('pwm_generator_harness/DUT/counters/PWM_TYPE', 'Value');
+end
+
 if strcmp(Data.TYPE,'TYPE.CENTERED'), K = 2;
 else,                                 K = 1;
 end
@@ -13,4 +18,4 @@ end
 PRESCALER.PWM = round((1e9/Data.PWM_FREQ)/(1e9/Data.CLK_FREQ)/(CNT_MAX+1)/K);
 
 %%
-clear Data CNT_MAX K
+clear CNT_MAX K
--- tests/createTestSignalsForPWM.m
+++ tests/createTestSignalsForPWM.m
@@ -1,40 +1,36 @@
-Data = extractdatafromdict();
-
 %%
 N = [4 6 8];
 Interval = N./Data.PWM_FREQ;
 
-%% UP
-u = uint8(zeros(2^Data.SIZE,3));
-t = zeros(2^Data.SIZE,3);
-ds = cell(1,3);
-for i = 1:3
-    u(:,i) = uint8(0:2^Data.SIZE-1);
-    t(:,i) = 0:Interval(i):(length(u)-1)*(Interval(i));
-    ds{i} = timeseries(u(:,i),t(:,i));
-end
+switch counterType
+    case 'UP'
+        u = uint8(zeros(2^Data.SIZE,3));
+        t = zeros(2^Data.SIZE,3);
+        dc = cell(1,3);
 
-%% UP & DOWN
-Pause = 3*Interval(1);
+        for i = 1:3
+            u(:,i) = uint8(0:2^Data.SIZE-1);
+            t(:,i) = 0:Interval(i):(length(u)-1)*(Interval(i));
+            dc{i} = timeseries(u(:,i),t(:,i));
+        end
 
-u = uint8(zeros(2*(2^Data.SIZE),3));
-t = zeros(2*(2^Data.SIZE),3);
-ds = cell(1,3);
+        Tsim = 0.006;
+    case 'REVERSE'
+        Pause = 3*Interval(1);
 
-% u(:,1) = uint8([0:2^Size-1 2^Size-1:-1:0]);
-% t(1:8,1) = 0:Interval(1):(length(u)/2-1)*(Interval(1));
-% t(9,1) = t(8,1)+Pause;
-% t(:,1) = [t(1:9,1); ...
-%     transpose(t(9,1)+Interval(1):Interval(1):(t(9,1)+(length(u)/2-1)*(Interval(1))))];
-% ds{1} = timeseries(u(:,1),t(:,1));
+        u = uint8(zeros(2*(2^Data.SIZE),3));
+        t = zeros(2*(2^Data.SIZE),3);
+        dc = cell(1,3);
+        HalfLengthT = length(t)/2;
 
-HalfLengthT = length(t)/2;
+        for i = 1:3
+            u(:,i) = uint8([0:2^Data.SIZE-1 2^Data.SIZE-1:-1:0]);
+            t(1:HalfLengthT,i) = 0:Interval(i):(length(u)/2-1)*(Interval(i));
+            t(1+HalfLengthT,i) = t(HalfLengthT,i)+Pause;
+            t(:,i) = [t(1:(1+HalfLengthT),i); ...
+                transpose(t(1+HalfLengthT,i)+Interval(i):Interval(i):(t((1+HalfLengthT),i)+(length(u)/2-1)*(Interval(i))))];
+            dc{i} = timeseries(u(:,i),t(:,i));
+        end
 
-for i = 1:3
-    u(:,i) = uint8([0:2^Data.SIZE-1 2^Data.SIZE-1:-1:0]);
-    t(1:HalfLengthT,i) = 0:Interval(i):(length(u)/2-1)*(Interval(i));
-    t(1+HalfLengthT,i) = t(HalfLengthT,i)+Pause;
-    t(:,i) = [t(1:(1+HalfLengthT),i); ...
-        transpose(t(1+HalfLengthT,i)+Interval(i):Interval(i):(t((1+HalfLengthT),i)+(length(u)/2-1)*(Interval(i))))];
-    ds{i} = timeseries(u(:,i),t(:,i));
+        Tsim = 0.011;
 end
